dea7f8fafb03a7f870bf87c91b14ccf6d34af001
André Rodríguez <rod18332@uvg.edu.gt>
2019-05-23 14:20:41 -0600

    2do commit

--- Encuesta.py
+++ Encuesta.py
@@ -7,12 +7,29 @@ from numpy import *
 #db = GraphDatabase("http://localhost:7474", username="neo4j", password="1234")
 
 def main():
+    mal = "Ingrese un valor correcto"
     print("Bienvenido BookRec\nProcederemos a Realizarle una serie de preguntas con las cuales podemos determinar los libros mas "
           "de ahuevo para usted.\nAntes de inciar la encuesta tomaremos unos datos de usted. Los datos no seran compartidos a"
           " terceros.")
-    numero = input("¿Cuál es tu nombre? ")
-    edad = input("¿Qué edad tiene actualmente? ")
-    genero = input("¿Con qué genero se identifica? ")
-    niveleducativo= input("¿Cual es su nivel educativo?\na. Bachillerato\nb. Licenciatura\nc. Posgrado")
-
+    banderaedad = False
+    nombre = genero = niveleducativo = ""
+    edad = 0
+    while (nombre.strip()==""):
+        nombre = input("¿Cuál es tu nombre? ").strip()
+        if(nombre == ""):
+            print(mal)
+    while (banderaedad):
+        try:
+            edad = int(input("¿Qué edad tiene actualmente? "))
+            banderaedad=False
+        except:
+            print(mal)
+    while (genero.strip()==""):
+        genero = input("¿Con qué genero se identifica? ").strip()
+        if(genero == ""):
+            print(mal)
+    while (niveleducativo.strip() == "" or (niveleducativo !="a" and niveleducativo!="b" and niveleducativo!="c")):
+        niveleducativo= input("¿Cual es su nivel educativo?\na. Bachillerato\nb. Licenciatura\nc. Posgrado").lower()
+        if(niveleducativo =="" or (niveleducativo !="a" and niveleducativo!="b" and niveleducativo!="c")):
+            print(mal)
 main()
